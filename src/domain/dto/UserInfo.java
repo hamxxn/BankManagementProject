5a1b706c968d0b1e313023e01e5acbb734701488
slr-09 <ge12090@naver.com>
2024-10-07 18:31:50 +0900

    userinfo

--- src/domain/dto/UserInfo.java
+++ src/domain/dto/UserInfo.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -15,11 +16,12 @@ public class UserInfo {
     private String phoneNum;
     private List<AccountInfo> accounts;
 
-    public UserInfo(String id, String username, int password, String phoneNum, String birth) {
+    public UserInfo(String id, String username, int password, String phoneNum, String birth, List<AccountInfo> accounts) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.phoneNum = phoneNum;
         this.birth = birth;
+        this.accounts = accounts;
     }
 }
